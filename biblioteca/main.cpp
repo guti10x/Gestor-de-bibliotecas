7565c628bdf37291fa0b4f5a8c721b60cf8666fe
HandImpersonator <96979322+HandImpersonator@users.noreply.github.com>
2022-10-06 01:20:59 +0200

    Añadidas todas las funciones
    Falta guardar cambios a CSV.

--- biblioteca/main.cpp
+++ biblioteca/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <fstream>
-#include <string.h>
+#include <string>
 #include <utility>
 #include <vector>
 #include <sstream>
@@ -51,6 +51,12 @@ public:
     string Disponible() {
         return prestado;
     }
+
+    void Cambiar_prestado(string prest) {
+        prestado = std::move(prest);
+    }
+
+
 };
 
 
@@ -106,6 +112,10 @@ public:
     string Historial() {
         return historial;
     }
+
+    void Cambiar_actual(string posesion) {
+        libro_en_posesion = std::move(posesion);
+    }
 };
 
 
@@ -153,7 +163,7 @@ private:
     vector<Libro> libros;
     vector<Historial> historiales;
 public:
-    Biblioteca() {};
+    Biblioteca() = default;
 
     void guardar_libros(int id, const string &t, const string &i, const string &c, const string& d) {
         libros.emplace_back(id, t, i, c, d);
@@ -167,46 +177,145 @@ public:
         clientes.emplace_back(id, n, a, d, ac, h);
     };
 
+    void asignar_libro(int id) {
+        string name = clientes[id - 1].Cliente::Nombre();
+        string surname = clientes[id - 1].Cliente::Apellido();
+        string dni = clientes[id - 1].Cliente::DNI();
+        if (clientes[id - 1].Cliente::Libro_actual() == "Ninguno"){
+            cout << "Asignar nuevo libro a cliente: " << name << " " << surname << endl;
+            print_libros_disponibles();
+            int new_book_id = 0;
+            cout << "Elegir ID de nuevo libro a asignar: ";
+            cin >> new_book_id;
+            cout << endl;
+            guardar_historiales(dni, libros[new_book_id - 1].Libro::Titulo(), libros[new_book_id - 1].Libro::ISBN(), libros[new_book_id - 1].Libro::Categoria());
+            cout << "Asignado nuevo libro: " << endl;
+            cout << "Título: " << libros[new_book_id - 1].Libro::Titulo() << endl;
+            cout << "ISBN: " << libros[new_book_id - 1].Libro::ISBN() << endl;
+            cout << "Categoría: " << libros[new_book_id - 1].Libro::Categoria() << endl;
+            cout << endl;
+            libros[new_book_id - 1].Cambiar_prestado("0");
+        }
+        else {
+            cout << "Usuario ya tiene libro asignado, volviendo al menú." << endl;
+            cout << endl;
+        }
+
+    };
+
+    void desasignar_libro(int id) {
+        string name = clientes[id - 1].Cliente::Nombre();
+        string surname = clientes[id - 1].Cliente::Apellido();
+        string dni = clientes[id - 1].Cliente::DNI();
+        if (clientes[id - 1].Cliente::Libro_actual() != "Ninguno"){
+            cout << "Desaignar libro a cliente: " << name << " " << surname << endl;
+            string old_book_name = clientes[id - 1].Cliente::Libro_actual();
+            int old_book_id = 0;
+            int i = 0;
+            while (i < libros.size()){
+                if (libros[i].Libro::Titulo() == old_book_name){
+                    old_book_id = libros[i].Libro::ID();
+                }
+                ++i;
+            }
+            guardar_historiales(dni, libros[old_book_id - 1].Libro::Titulo(), libros[old_book_id - 1].Libro::ISBN(), libros[old_book_id - 1].Libro::Categoria());
+            cout << "Desasignado libro: " << endl;
+            cout << "Título: " << libros[old_book_id - 1].Libro::Titulo() << endl;
+            cout << "ISBN: " << libros[old_book_id - 1].Libro::ISBN() << endl;
+            cout << "Categoría: " << libros[old_book_id - 1].Libro::Categoria() << endl;
+            cout << endl;
+            clientes[id - 1].Cambiar_actual("Ninguno");
+            libros[old_book_id - 1].Cambiar_prestado("1");
+        }
+        else {
+            cout << "Usuario no tiene libro asignado, volviendo al menú." << endl;
+            cout << endl;
+        }
+    };
+
     void print_libros() {
         cout << "Libros: " << endl;
-        for (int i = 0; i < libros.size(); ++i) {
+        int i = 0;
+        while (i < libros.size()) {
             cout << "ID: " << libros[i].Libro::ID() << endl;
             cout << "Título: " << libros[i].Libro::Titulo() << endl;
             cout << "ISBN: " << libros[i].Libro::ISBN() << endl;
-            cout << "Categoría: " << libros.at(i).Libro::Categoria() << endl;
-            cout << "Disponible: " << libros.at(i).Libro::Disponible() << endl;
+            cout << "Categoría: " << libros[i].Libro::Categoria() << endl;
+            cout << "Disponible: " << libros[i].Libro::Disponible() << endl;
             cout << endl;
             cout << endl;
+            ++i;
+        }
+    };
+
+    void print_libros_disponibles() {
+        cout << "Libros: " << endl;
+        int i = 0;
+        while (i < libros.size()) {
+            if (libros[i].Libro::Disponible() == "1"){
+                cout << "ID: " << libros[i].Libro::ID() << endl;
+                cout << "Título: " << libros[i].Libro::Titulo() << endl;
+                cout << "ISBN: " << libros[i].Libro::ISBN() << endl;
+                cout << "Categoría: " << libros[i].Libro::Categoria() << endl;
+                cout << "Disponible: " << libros[i].Libro::Disponible() << endl;
+                cout << endl;
+            }
+            ++i;
         }
     };
 
     void print_clientes() {
         cout << "Clientes: " << endl;
-        for (int i = 0; i < clientes.size(); ++i) {
+        int i = 0;
+        while (i < clientes.size()) {
             cout << "ID: " << clientes[i].Cliente::ID() << endl;
             cout << "Nombre: " << clientes[i].Cliente::Nombre() << endl;
             cout << "Apellido: " << clientes[i].Cliente::Apellido() << endl;
-            cout << "DNI: " << clientes.at(i).Cliente::DNI() << endl;
-            cout << "Libro en posesion: " << clientes.at(i).Cliente::Libro_actual() << endl;
+            cout << "DNI: " << clientes[i].Cliente::DNI() << endl;
+            cout << "Libro en posesion: " << clientes[i].Cliente::Libro_actual() << endl;
             cout << "Historial: " << clientes[i].Cliente::Historial() << endl;
             cout << endl;
             cout << endl;
+            ++i;
         }
+    };
+
+    void print_cliente(int id) {
+        string name = clientes[id - 1].Cliente::Nombre();
+        string surname = clientes[id - 1].Cliente::Apellido();
+        string dni = clientes[id - 1].Cliente::DNI();
+        cout << "Cliente: " << name << " " << surname << endl;
+        cout << "ID: " << clientes[id - 1].Cliente::ID() << endl;
+        cout << "Nombre: " << clientes[id - 1].Cliente::Nombre() << endl;
+        cout << "Apellido: " << clientes[id - 1].Cliente::Apellido() << endl;
+        cout << "DNI: " << clientes[id - 1].Cliente::DNI() << endl;
+        cout << "Libro en posesion: " << clientes[id - 1].Cliente::Libro_actual() << endl;
+        cout << "Historial: " << clientes[id - 1].Cliente::Historial() << endl;
+        cout << endl;
+        cout << endl;
     };
 
     void print_historial_cliente(int id) {
 
         string name = clientes[id - 1].Cliente::Nombre();
         string surname = clientes[id - 1].Cliente::Apellido();
-        string history = clientes[id - 1].Historial();
         cout << "Historial Cliente " << name << " " << surname << endl;
-        for (int i = 0; i < historiales.size(); ++i) {
+        int i = 0;
+        while (i < historiales.size()) {
             if (historiales[i].Historial::ID_h() == clientes[id - 1].Cliente::DNI()){
+                int j = 0;
+                while (j < libros.size()){
+                    if (libros[j].Libro::ISBN() == historiales[i].Historial::ISBN_h()){
+                        cout << "ID: " << libros[j].Libro::ID() << endl;
+                    }
+                    ++j;
+                }
                 cout << "Título: " << historiales[i].Historial::Titulo_h() << endl;
                 cout << "ISBN: " << historiales[i].Historial::ISBN_h() << endl;
                 cout << "Categoría: " << historiales[i].Historial::Categoria_h() << endl;
                 cout << endl;
             }
+            ++i;
         }
     };
 };
@@ -298,9 +407,15 @@ int main() {
     }
     file3.close();
 
-    biblioteca.print_clientes();
-    biblioteca.print_libros();
-    biblioteca.print_historial_cliente(1);
+    // biblioteca.print_clientes(); // Imprime todos los clientes.
+    // biblioteca.print_libros(); // Imprime todos los libros.
+    // biblioteca.print_historial_cliente(1); // Imprime el historial de un cliente.
+    // biblioteca.print_libros_disponibles(); // Imprime todos los libros disponibles.
+    // biblioteca.asignar_libro(1); // Permite asignar un libro a un usuario.
+    // Imprime la lista de libros disponibles para elegir, parámetro de entrada ID del cliente a asignar el libro.
+    // biblioteca.print_historial_cliente(1); // Imprime el historial de un cliente. Puesto para comprobar que se asignaba.
+    // biblioteca.print_cliente(1); // Imprime un cliente. Parámentro de entrada ID del cliente.
+    // biblioteca.desasignar_libro(1); // Desasignar un libro a un cliente. Parámetro de entrada ID del cliente.
 
     return 0;
 }
